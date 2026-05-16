69df8521420f438fc56d6a0167623f15501826e1
Luis Corimanya <lcorimanya_o@hotmail.com>
2026-05-15 19:01:05 -0500

    Primer commit

---
+++ CV2.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.9.0/dist/css/foundation.min.css">
+    <link href="CSS_CV.css" rel="stylesheet" type="text/css"/>
+    <title>Corimanya Osorio Luis</title>
+</head>
+<body>
+    <header>
+        <strong>953010889 | 2391206 | lcorimanya_o@hotmail.com</strong>
+        <nav>
+            <ul>
+                <li><a href="#perfil">Perfil</a></li>
+                <li><a href="#estudios">Estudios</a></li>
+                <li><a href="#conocimientos">Conocimientos</a></li>
+                <li><a href="#habilidades">Habilidades</a></li>
+                <li><a href="#intereses">Intereses</a></li>
+                <li><a href="#contacto">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section id="perfil" class="perfil">
+        <div class="perfil-contenedor">
+            <div class="perfil-imagen">
+                <img src="0333212008.jpg" alt="Foto de perfil"/>
+            </div>
+            <div class="perfil-info">
+                <h2 class="perfil-nombre">Luis Andre Corimanya Osorio</h2>
+                <p class="perfil-descripcion">
+                    Soy un estudiante de la carrera profesional Ingenieria Informatica y
+                    tambien cargador funebre de la Funeraria San Pedro de Huacho(la mejor),
+                    hago mantenimiento de fontaneria(en mi casa), electricista(en casa),
+                    conexion de redes. En estudios tengo certificado UNI de Fundamentos en PHP,
+                    Analisis de Datos con Excel, Fundamentos de JavaScript y SQL Server.
+                    Soy una persona que le gusta trabajar en equipo(que todos aporten), aprender
+                    de otros, formado en valores.
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                </p>
+            </div>
+        </div>
+    </section>
+    <section id="estudios" class="estudios">
+        <div class="estudio-uno">
+            <div class="estudio-uno-imagen">
+                <img src="imgPrimaria.png" alt="Foto Primaria">
+            </div>
+            <div class="estudio-uno-nombre">
+                <p class="descripcion-uno">
+                    I.E.P. Maria Auxiliadora
+                    <br>
+                    Primaria
+                    <br>
+                    2010-2015
+                </p>
+            </div>
+        </div>
+        <div class="estudio-dos">
+            <div class="estudio-dos-imagen">
+                <img src="imgSecundaria.png" alt="Foto Secundaria">
+            </div>
+            <div class="estudio-dos-nombre">
+                <p class="descripcion-dos">
+                    I.E.P. San Ignacio de Loyola
+                    <br>
+                    Secundaria
+                    <br>
+                    2016-2020
+                </p>
+            </div>
+        </div>
+        <div class="estudio-tres">
+            <div class="estudio-tres-imagen">
+                <img src="imgSuperior.jpg" alt="Foto Superior">
+            </div>
+            <div class="estudio-tres-nombre">
+                <p class="descripcion-tres">
+                    Universidad Nacional Jose 
+                    <br>
+                    Faustino Sanchez Carrion
+                    <br>
+                    Superior
+                    <br>
+                    2022-2027(en teoria)
+                </p>
+            </div>
+        </div>
+    </section>
+    <section id="conocimientos" class="conocimientos">
+        <!--<h2 class="titulos-cono">Conocimientos</h2>-->
+        <div class="conocimiento-uno">
+            <div class="conocimiento-uno-nombre">
+                <p class="descripcion-uno-cono">
+                    Conocimientos en SQL Server
+                </p>
+                <p>Conceptos basicos, creacion y gestion de BDs,<br>
+                   diseño de tablas e indices, DML, ensambles, <br>
+                   consultas avanzadas, vistas y optimizacion.
+                </p>
+            </div>
+        </div>
+        <div class="conocimiento-dos">
+            <div class="conocimiento-dos-nombre">
+                <p class="descripcion-dos-cono">
+                    Conocimientos en Conexion de Redes
+                </p>
+                <p>Fundamentos y arquitectura, cableado estructurado,<br>
+                    redes IP, servicios y seguridad de redes, control <br>
+                    de escritorios remotos.
+                </p>
+            </div>
+        </div>
+        <div class="conocimiento-tres">
+            <div class="conocimiento-tres-nombre">
+                <p class="descripcion-tres-cono">
+                    Conocimientos en Evaluacion de <br>
+                    Proyectos Informaticos
+                </p>
+                <p>Identificacion y formulacion, gestion de tiempos y <br>
+                recursos, evaluacion economica, auditoria.
+                </p>
+            </div>
+        </div>
+    </section>
+    <section id="habilidades">
+        <h2 class="titulos">Habilidades Tecnicas</h2>
+        <div class="contenedor-habilidades">
+            <div class="habilidad">
+                <span>PHP</span>
+                <div class="barra"><div class="progreso" style="width: 60%;">60%</div></div>
+            </div>
+            <div class="habilidad">
+                <span>JavaScript</span>
+                <div class="barra"><div class="progreso" style="width: 40%;">40%</div></div>
+            </div>
+            <div class="habilidad">
+                <span>Analisis de Datos</span>
+                <div class="barra"><div class="progreso" style="width: 50%;">50%</div></div>
+            </div>
+            <div class="habilidad">
+                <span>SQL SERVER 2025</span>
+                <div class="barra"><div class="progreso" style="width: 60%;">60%</div></div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <p class="titulo-interes">Intereses</p>
+        <p class="interes-info"><q><i>Me gustaria incorporarme a la parte de <u>soporte tecnico</u>, <u>testeo</u> y <u>pruebas en tiempo real</u>, <u>auditorias</u> para controlar quien
+            entra y quien sale. Meterme en el sector logistico de una empresa para monitorear los recursos habiles.</i></q>
+        </p>
+    </section>
+    <section>
+        <h2>|Introduce tus datos</h2>
+        <form class="info-contacto">
+            <label class="info-uno">
+                Nombre
+                <input type="text">
+            </label>
+            <br>
+            <label class="info-uno">
+                Apellido 1
+                <input type="text">
+            </label>
+            <br>
+            <label class="info-uno">
+                Apellido 2
+                <input type="text">
+            </label>
+            <br>
+            <label class="info-uno">
+                Telefono
+                <input type="text">
+            </label>
+            <br>
+            <label class="info-uno">
+                Email
+                <input type="text">
+            </label>
+            <br>
+            <label class="info-uno">
+                Confirmar Email
+                <input type="text">
+            </label>
+            <br>
+            <input type="submit" value="Enviar">
+        </form>
+    </section>
+    <footer>
+        <div>
+            <p>
+                <i><b>&copy Universidad Nacional: Jose Faustino Sanchez Carrión</b></i>
+                <br>Facultad Ingeniería Industrial, Sistemas e Informática<br>
+                Escuela Profesional Ingeniería Informática
+            </p>
+        </div>
+        <div>
+            <p>
+                <b>Estudiante</b>: Corimanya Osorio Luis Andre
+                <br>
+                <b>Docente</b>: Ing. Ronald Flores Flores
+            </p>
+        </div>
+        <div>
+            <p>
+                <b>Curso</b>: Desarrollo de Aplicaciones Web
+                <br>
+                <b>Ciclo</b>: IX
+            </p>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ CV_Cori.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV Corimanya Osorio Luis</title>
+    <!--Foundation-->
+    <link rel="stylesheet" href="css/foundation.min.css">
+    <link href="CSS_CV.css" rel="stylesheet"/>
+</head>
+<body>
+    <header class="top-bar">
+        <div class="top-bar-left">
+            <strong><i>953010889 | 2391206 | lcorimanya_o@hotmail.com</i></strong>
+        </div>
+        <div class="top-bar-right">
+            <ul class="dropdown menu" data-dropdown-menu>
+                <li><a href="#perfil">Perfil</a></li>
+                <li><a href="#estudios">Estudios</a></li>
+                <li><a href="#conocimientos">Conocimientos</a></li>
+                <li><a href="#habilidades">Habilidades</a></li>
+                <li><a href="#intereses">Intereses</a></li>
+                <li><a href="#contacto">Contacto</a></li>
+            </ul>
+        </div>
+    </header>
+    <div class="grid-container principal">
+        <div class="grid-x grid-margin-x align-middle">
+            <div class="cell small-12 medium-7">
+                <h1>Luis Andre Corimanya Osorio</h1>
+                <p>
+                    Estudiante de la carrera profesional Ingenieria Informatica
+                </p>
+            </div>
+            <div class="cell small-12 medium-5 text-center">
+                <img src="0333212008.jpg" class="perfil-img"/>
+            </div>
+        </div>
+    </div>
+    <div class="grid-container">
+        <section id="perfil" class="callout perfil">
+            <h2 class="text-center">Perfil</h2>
+            <p class="text center">
+                    Soy un estudiante de la carrera profesional Ingenieria Informatica y
+                    tambien cargador funebre de la Funeraria San Pedro de Huacho(la mejor),
+                    hago mantenimiento de fontaneria(en mi casa), electricista(en casa),
+                    conexion de redes. En estudios tengo certificado UNI de Fundamentos en PHP,
+                    Analisis de Datos con Excel, Fundamentos de JavaScript y SQL Server.
+                    Soy una persona que le gusta trabajar en equipo(que todos aporten), aprender
+                    de otros, formado en valores.
+            </p>
+        </section>
+
+        <section id="estudios">
+            <h2 class="text-center">Estudios Realizados</h2>
+            <div class="grid-x grid-margin-x align-stretch">
+                <!-- PRIMARIA -->
+                <div class="cell small-12 medium-4">
+                    <div class="card">
+                        <img src="imgPrimaria.png">
+                        <div class="card-section">
+                            <h4>Primaria</h4>
+                            <p>Maria Auxiliadora</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- SECUNDARIA -->
+                <div class="cell small-12 medium-4">
+                    <div class="card">
+                        <img src="imgSecundaria.png">
+                        <div class="card-section">
+                            <h4>Secundaria</h4>
+                            <p>San Ignacio de Loyola</p>
+                        </div>
+                    </div>
+                </div>
+
+            <!-- UNIVERSIDAD -->
+                <div class="cell small-12 medium-4">
+                    <div class="card">
+                        <img src="imgSuperior.jpg">
+                        <div class="card-section">
+                            <h4>Universidad</h4>
+                            <p>Universidad Nacional José Faustino Sánchez Carrión</p>
+                        </div>
+                    </div>
+                </div>  
+            </div>
+        </section>
+        <section id="conocimientos" class="callout">
+            <h2 class="text-center">Conocimientos</h2>
+            <div class="grid-x grid-margin-x small-up-2 medium-up-5">
+                <div class="cell">
+                    <div class="skill-box">Conocimientos en SQL Server</div>
+                </div>
+                <div class="cell">
+                    <div class="skill-box">Conocimientos en Conexion de Redes</div>
+                </div>
+                <div class="cell">
+                    <div class="skill-box">Conocimientos en Evaluacion de <br>
+                                            Proyectos Informaticos
+                    </div>
+                </div>
+                <div class="cell">
+                    <div class="skill-box">Conocimientos en HTML</div>
+                </div>
+                <div class="cell">
+                    <div class="skill-box">Conocimientos en Infraestructura de Redes</div>
+                </div>
+        </section>
+        <section id="habilidades">
+            <h2 class="text-center">Habilidades Tecnicas</h2>
+            <div class="grid-x grid-margin-x align-stretch">
+                <div class="cell small-12 medium-4">
+                    <div class="card text-center">
+                        <img src="php.jpg">
+                        <div class="card-section">
+                            <h4>PHP</h4>
+                            <progress value="60" max="100"></progress>
+                        </div>
+                    </div>
+                </div>
+                <div class="cell small-12 medium-4">
+                    <div class="card text-center">
+                        <img src="html.jpg">
+                        <div class="card-section">
+                            <h4>HTML</h4>
+                            <progress value="50" max="100"></progress>
+                        </div>
+                    </div>
+                </div>
+                <div class="cell small-12 medium-4">
+                    <div class="card text-center">
+                        <img src="sql.jpg">
+                        <div class="card-section">
+                            <h4>SQL SERVER 2025</h4>
+                            <progress value="60" max="100"></progress>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="intereses" class="callout">
+            <h2 class="text-center">Intereses</h2>
+            <div class="text-center grid-x grid-margin-x small-up-2 medium-up-3">
+                <div class="cell">
+                    <div class="skill-box">Capacitación en Auditorias</div>
+                    <p>Manejar controles de calidad, mejorar la seguridad y organización de la empresa.</p>
+                </div>
+                <div class="cell">
+                    <div class="skill-box">Capacitación en Area de Soporte Tecnico</div>
+                    <p>Conocer sobre testeos, pruebas a softwares y/o trabajadores tipo capacitarlos</p>
+                </div>
+                <div class="cell">
+                    <div class="skill-box">Capacitación en Sector Logistico</div>
+                    <p>Tener control en lo que entra y sale en el inventario, hacer reportes etc</p>
+                </div>
+            </div>
+        </section>
+        <section id="contacto">
+            <div class="grid-x align-center">
+                <div class="cell small-12 medium-8 large-6">
+                    <div class="callout">
+                        <h2 class="text-center">Informacion de Contacto</h2>
+                        <form>
+                            <label>Nombres<input type="text"></label>
+                            <label>Apellidos<input type="text"></label>
+                            <label>Cargo<input type="text"></label>
+                            <label>Consulta<textarea rows="5"></textarea></label>
+                            <div class="text-center">
+                                <button type="submit" class="button success">Enviar</button>
+                                <button type="reset" class="button alert">Borrar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <footer class="pie text-center">
+        <div>
+            <p>
+                <i><b>&copy Universidad Nacional: Jose Faustino Sanchez Carrión</b></i>
+                <br>Facultad Ingeniería Industrial, Sistemas e Informática<br>
+                Escuela Profesional Ingeniería Informática
+            </p>
+        </div>
+        <div>
+            <p>
+                <b>Estudiante</b>: Corimanya Osorio Luis Andre
+                <br>
+                <b>Docente</b>: Ing. Ronald Flores Flores
+            </p>
+        </div>
+        <div>
+            <p>
+                <b>Curso</b>: Desarrollo de Aplicaciones Web
+                <br>
+                <b>Ciclo</b>: IX
+            </p>
+        </div>
+    </footer>
+</body>
+</html>
